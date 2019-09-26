5c746f4fddb45e952140dd7402376b1439d7aeed
Matheus Gabriel Moreira <matheus.moreira@mjv.com.br>
2019-09-26 14:51:18 -0300

    v0.1.0

---
+++ app/controllers/jogo.js
@@ -0,0 +1,9 @@
+module.exports.jogo = function (application, req, res) {
+    
+    if(req.session.autorizado){
+        res.render('jogo');
+    }else {
+        res.render('index', {validacao: {}}); 
+    }
+
+}
---
+++ config/dbConnection.js
@@ -0,0 +1,19 @@
+var mongo = require('mongodb');
+
+var connMongoDB = function() {
+    var db = new mongo.Db(
+        'got',
+        new mongo.Server(
+            'localhost', //String contendo o endereço do servidor
+            27017, //portão de conexão
+            {}
+        ),
+        {}
+    );
+
+    return db;
+}
+
+module.exports = function () {
+    return connMongoDB;
+}
